0343888ba393fb571336860e7bffa24cce1e179e
f.barbano <f.barbano@reply.it>
2018-11-06 23:50:06 +0100

    start

--- tm-lyrics-downloader/src/test/java/common/TestUtil.java
+++ tm-lyrics-downloader/src/test/java/common/TestUtil.java
@@ -5,5 +5,5 @@ import java.nio.file.Paths;
 
 public class TestUtil {
 
-    public static final Path RESOURCES_TEST_FOLDER = Paths.get("C:\\Users\\feder\\IdeaProjects\\APPS\\tagmod\\tm-lyrics-downloader\\src\\test\\resources");
+    public static final Path RESOURCES_TEST_FOLDER = Paths.get(System.getProperty("user.home")).resolve("IdeaProjects\\APPS\\tagmod\\tm-lyrics-downloader\\src\\test\\resources");
 }
